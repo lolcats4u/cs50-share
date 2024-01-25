84030ffa14c50ca47daa4b58fecc421b2facfa3e
lolcats4u <kittiesnmore@gmail.com>
2024-01-24 16:36:37 -0800

    syntactic sugar

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -67,15 +67,15 @@ int average(const RGBTRIPLE pixel)
 
 void make_sepia_pixel(RGBTRIPLE *pixel)
 {
-    const RGBTRIPLE reference = {.rgbtBlue = *pixel.rgbtBlue, .rgbtGreen = *pixel.rgbtGreen, .rgbtRed = *pixel.rgbtRed};
+    const RGBTRIPLE reference = {.rgbtBlue = pixel->rgbtBlue, .rgbtGreen = pixel->rgbtGreen, .rgbtRed = pixel->rgbtRed};
 
     BYTE blue = hex_cap_and_byte(.272 * reference.rgbtRed + .534 * reference.rgbtGreen + .131 * reference.rgbtBlue);
     BYTE green = hex_cap_and_byte(.349 * reference.rgbtRed + .686 * reference.rgbtGreen + .168 * reference.rgbtBlue);
     BYTE red = hex_cap_and_byte(.393 * reference.rgbtRed + .769 * reference.rgbtGreen + .189 * reference.rgbtBlue);
     
-    *pixel.rgbtBlue = blue;
-    *pixel.rgbtGreen = green;
-    *pixel.rgbtRed = red;
+    pixel->rgbtBlue = blue;
+    pixel->rgbtGreen = green;
+    pixel->rgbtRed = red;
 
     return;
 }
